eca827adf27fe45c7767fdc99282dd16d6cfad18
erobakiewicz <erobakiewicz@gmail.com>
2020-10-02 19:37:21 +0200

    heroku setup files

--- BookShop/settings.py
+++ BookShop/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'b-8x4es-@c82=@mzizw748vy#%_j91*c4%*lbn^lhxm-b^r6xl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['ebookstorebookers.herokuapp.com','127.0.0.1']
 
 # Application definition
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,5 +121,6 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/templates/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Bookies/static/img/')
 MEDIA_URL = '/media/'
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn BookShop.wsgi
+release: python manage.py migrate
